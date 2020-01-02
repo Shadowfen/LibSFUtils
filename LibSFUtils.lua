912d38833ce1a5f546b9b1115d4f5571564baef9
Dolores Scott <shadowfen@gmail.com>
2020-01-02 15:25:37 -0600

    version 21

--- LibSFUtils.lua
+++ LibSFUtils.lua
@@ -1,4 +1,6 @@
-LibSFUtils = {}
+LibSFUtils = {
+    LibVersion = 21,    -- change this with every release!
+}
 
 sfutil = LibSFUtils
 
@@ -180,6 +182,13 @@ end
   where the original function is defined). If the namespace parameter is not provided
   then assume the global namespace _G.
   
+  The wrapped function cannot be local - I mean, what's the point?
+  
+  Parameters:
+    namespace - (optional) when provided, this is a table where the function to be wrapped resides. If not provided, the global namespace _G is assumed.
+    functionName - a string with the name of the function to be wrapped (used as a key to the namespace table)
+    wrapper - a function which accepts a function, followed by the parameters that the original function expects to be provided. Therefore the wrapped function can call the original function if it wishes to.
+  
   Examples:
     WrapFunction(myfunc, mywrapper)
       will wrap the global function myfunc to call mywrapper
@@ -188,13 +197,15 @@ end
       will wrap TT.myfunc with a call to mywrapper
 ]]
 function sfutil.WrapFunction(namespace, functionName, wrapper)
-  
-    if(type(namespace) == "string") then
+    if type(namespace) == "string"  then
         -- We did not get a namespace parameter,
         -- shift the values to their proper places.
         wrapper = functionName
         functionName = namespace
         namespace = _G
+    elseif type(namespace) ~= "table" then
+        -- invalid parameters
+        return nil
     end
     local originalFunction = namespace[functionName]
     namespace[functionName] = function(...) 
@@ -207,6 +218,7 @@ end
 -- a defaults table. Existing values in the svtable will
 -- remain unchanged.
 function sfutil.defaultMissing(svtable, defaulttable)
+    if svtable == nil then return sfutil.deepCopy(defaulttable) end
 	if type(svtable) ~= 'table' then return end
 	if type(defaulttable) ~= 'table' then return end
 	
@@ -536,6 +548,96 @@ function sfutil.LoadLanguage(lang_strings, defaultLang)
         end
     end
 end
+
+-- -----------------------------------------------------------------------
+-- sound choice utilities
+
+-- initSounds() is the only function to modify these tables in any way,
+-- and they are never nil.
+local soundChoices = {}     -- contains [index] value=SOUNDS.xxx
+local soundReverse = {}     -- contains [SOUNDS.xxx] index
+
+-- This function loads a pair of "singleton" arrays of sounds
+-- from the ZOS SOUNDS map
+--
+-- An addon may call this (on loading), or may rely on the
+-- other sfutil sound functions to call it. If it has already
+-- run once, it does not do so again.
+--
+function sfutil.initSounds(force)
+    if force == true then soundChoices={} end
+    -- load sound keys to table
+    if next(soundChoices) == nil then  
+        for k,_ in pairs(SOUNDS) do
+            local ndx = #soundChoices+1
+            soundChoices[ndx] = k
+        end
+        table.sort(soundChoices)
+        for i,k in pairs(soundChoices) do
+            soundReverse[k] = i
+        end
+    end
+end
+
+-- provide the max number of sounds provided by ZOS
+-- that were loaded into the soundChoices array
+--
+function sfutil.numSounds()
+    sfutil.initSounds()
+    return #soundChoices
+end
+
+-- Get the soundChoices index number for a SOUNDS.xxx value.
+--
+-- Useful for setting a slider value from a saved (string) sound entry.
+function sfutil.getSoundIndex(sound, fallback)
+    if not fallback then fallback = SOUNDS.DEFAULT_CLICK end
+    
+    sfutil.initSounds()
+    if not soundReverse[fallback] then fallback = SOUNDS.DEFAULT_CLICK end
+    
+    local ndx = soundReverse[sound]
+    if not ndx then return soundReverse[fallback] end
+    return ndx
+end
+
+-- Given an index number, get the sound entry out of the
+-- soundChoices array
+function sfutil.getSound(index)
+    sfutil.initSounds()
+    local sound = soundChoices[index]
+    if not sound then return "nil" end
+    return sound
+end
+
+-- Play a sound selected by index number into a list 
+-- of all of the games sounds we have access to or SOUNDS.xxx entry.
+-- If the index is out-of-bounds or SOUNDS.xxx is nil, do not play a sound.
+--
+-- Note that at present, the SOUNDS array has TWO entries
+-- for "No_Sound", which will "play" silence.
+--
+function sfutil.PlaySound(index)
+    if not index then return end
+    
+    if type(index) == "number" then
+    	-- we have an index into our soundChoices array
+    	-- so, make sure it exists (once).
+    	sfutil.initSounds()
+        if soundChoices[index] == nil then return end
+        PlaySound(soundChoices[index])
+        
+    elseif type(index) == "string" then
+        if SOUNDS[index] ~= nil then
+        	-- we got a sound name (index into the SOUNDS array)
+            PlaySound(SOUNDS[index])
+            
+        else
+        	-- presume we have a string that was stored in a SOUNDS.xxx
+            PlaySound(index)
+        end
+    end
+end    
 
 -- -----------------------------------------------------------------------
 -- experimental functions - not ready for prime-time
