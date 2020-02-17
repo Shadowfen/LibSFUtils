7391260b29f63d5865554697f6f0705fa4fd08f0
Shadowfen <shadowfen@gmail.com>
2020-02-17 00:24:56 -0600

    Update changelog

---
+++ Experimental.lua
@@ -0,0 +1,63 @@
+-- LibSFUtils is already defined in prior loaded file
+
+sfutil = LibSFUtils
+
+-- -----------------------------------------------------------------------
+-- experimental functions - not ready for prime-time
+-- -----------------------------------------------------------------------
+
+
+function sfutil.getDLCs()
+	d("COLLECTIBLE_CATEGORY_TYPE_DLC = "..COLLECTIBLE_CATEGORY_TYPE_DLC)
+    for categoryIndex = 1, GetNumCollectibleCategories() do
+        local categoryName, numSubCategories, numCollectibles, unlockedCollectibles = 
+				GetCollectibleCategoryInfo(categoryIndex)
+		if(categoryName == "Stories") then
+			d("Name: "..categoryName.."  Index: "..categoryIndex)
+			d("Num subcategories: "..numSubCategories)
+			for subcategoryIndex = 1, numSubCategories do
+				local subCategoryName, subNumCollectibles, subNumUnlockedCollectibles = 
+					GetCollectibleSubCategoryInfo(categoryIndex, subcategoryIndex)
+				if( subCategoryName ~= nil ) then
+					d("->SubName: "..subCategoryName.."  index: "..subcategoryIndex)
+				else
+					d("->Sub category Index: "..subcategoryIndex)
+				end
+				d("->Num subcollectibles: "..subNumCollectibles)	
+				--
+				local formattedSubcategoryName = 
+					zo_strformat(SI_COLLECTIBLE_NAME_FORMATTER, subCategoryName)
+				for coll=1, subNumCollectibles do
+					local id = GetCollectibleId(categoryIndex, 
+						subcategoryIndex, coll)
+					local name, description, iconFile, deprecatedLockedIconFile, 
+						unlocked, purchasable, active, categoryType, hint, 
+						isPlaceholder = GetCollectibleInfo(id)
+					local unlockState = GetCollectibleUnlockStateById(id)
+					d("index: "..coll.."  id: "..id.."  Name: "..name.."  unlocked: "..sfutil.bool2str(unlocked).."  active: "..sfutil.bool2str(active))
+ 				end
+				--]]
+			end
+		end
+	end
+end
+
+function sfutil.getChapters()
+    local name, _, numCollectibles, unlockedCollectibles, _, _, collectibleCategoryType = GetCollectibleCategoryInfo(COLLECTIBLE_CATEGORY_TYPE_CHAPTER)
+     d("Number of Chapters: ".. numCollectibles)
+    for i=1, numCollectibles do
+        local collectibleId = GetCollectibleId(COLLECTIBLE_CATEGORY_TYPE_CHAPTER, nil, i)
+        local collectibleName, _, _, _, unlocked = GetCollectibleInfo(collectibleId) 
+			-- Will return true or false. If the user unlocked through ESO+ without buying DLC it will return true.
+        d(sfutil.str("DLC ", collectibleName, "( ", collectibleId, ") unlocked : ",
+			tostring(unlocked)))
+    end
+end
+
+function sfutil.gettags(bagId,slotId)
+	local itemLink = GetItemLink(bagId, slotId)
+    for i=1, GetItemLinkNumItemTags(itemLink) do
+        local itemTagDescription, itemTagCategory = GetItemLinkItemTagInfo(itemLink, i)
+		d(sfutil.str("slot: ",slotId,"  category: ",itemTagCategory,"  desc: ",itemTagDescription))
+    end
+end
---
+++ test/test_VersionChecker.lua
@@ -0,0 +1,77 @@
+require "LibSFUtils.test.tk"
+require "LibSFUtils.LibSFUtils_Global"
+require "LibSFUtils.LibSFUtils"
+require "LibSFUtils.SFUtils_VersionChecker"
+local TK = TestKit
+local SF = LibSFUtils
+
+local TR = test_run
+local d = print
+
+local moduleName = "VersionChecker"
+local mn = "VersionChecker"
+
+-- main
+TK.init()
+
+
+local vc = SF.VersionChecker("testVC")
+TK.assertNotNil(vc,"VC call create 1")
+--d(vc.addonName or "nil")
+TK.assertTrue(vc.addonName == "testVC","Addon name set 1")
+TK.assertFalse(vc.enabled,"No LibDebugLogger, so not enabled 1")
+vc = nil
+
+vc = SF.VersionChecker("testVC")
+TK.assertNotNil(vc,"VC call create 2")
+--d(vc.addonName or "nil")
+TK.assertTrue(vc.addonName == "testVC","Addon name set 2")
+TK.assertFalse(vc.enabled,"No LibDebugLogger, so not enabled 2")
+
+vc:Enable()
+TK.assertTrue(vc.enabled,"LibDebugLogger enabled")
+
+vc:Disable()
+TK.assertFalse(vc.enabled,"No LibDebugLogger, so not enabled 3")
+
+local testlogger = {
+    msg = {},
+    Error = function(self,...)  self.msg.error = string.format(...) end,
+    Warn = function(self,...)  self.msg.warn = string.format(...) end,
+    Info = function(self,...)  self.msg.info = string.format(...) end,
+    Debug = function(self,...)  self.msg.debug = string.format(...) end,
+}
+setmetatable(testlogger, { __call = function(self,name,logger) 
+            self.addonName = name 
+            self.logger = logger
+            return self
+        end
+    })
+vc = SF.VersionChecker("testVC", testlogger)
+vc:NoVersion("blah")
+TK.assertTrue(testlogger.msg.info == "Library \"blah\" does not provide version information", testlogger.msg.info or "test No Version failed")
+
+local function checkVer(libname)
+    local libtab = {
+        LibSFUtils = 23,
+        ["LibAddonMenu-2.0"] = 30,
+    }
+    return libtab[libname] or -1
+end
+testlogger.msg.error = nil
+vc:CheckVersion("LibSFUtils",checkVer, 24)
+TK.assertNotNil(testlogger.msg.error, testlogger.msg.error or "LibSFUtils version is not correct")
+
+testlogger.msg.error = nil
+vc:CheckVersion("LibSF",checkVer, 24)
+TK.assertNotNil(testlogger.msg.error, testlogger.msg.error or "LibSF was found")
+
+testlogger.msg.error = nil
+vc:CheckVersion("LibSFUtils",checkVer, 22)
+TK.assertNil(testlogger.msg.error, testlogger.msg.error or "LibSFUtils version is correct")
+
+
+
+d("\n")
+TK.showResult()
+d()
