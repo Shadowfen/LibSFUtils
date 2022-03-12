bce7b2ee634cafc4b46683f7a2f51af13b907c33
Dolores Scott <scottd@texas.net>
2022-03-12 12:50:09 -0600

    API bump

--- LibSFUtils_Global.lua
+++ LibSFUtils_Global.lua
@@ -3,6 +3,6 @@
 
 LibSFUtils = {
     name = "LibSFUtils",
-    LibVersion = 39,    -- change this with every release!
+    LibVersion = 40,    -- change this with every release!
     author = "Shadowfen",
 }
