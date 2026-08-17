54546ea1cc32397da0857ba2a53450eb3b61378b
Shadowfen <shadowfen@gmail.com>
2026-08-17 16:44:27 -0500

    version 76

--- LibSFUtils_Global.lua
+++ LibSFUtils_Global.lua
@@ -3,7 +3,7 @@
 
 LibSFUtils = {
     name = "LibSFUtils",
-    LibVersion = 75,    -- change this with every release!
+    LibVersion = 76,    -- change this with every release!
     author = "Shadowfen",
 }
 --[[
